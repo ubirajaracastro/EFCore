3e752eef8c4c8379f93c721e405c048dbab83f84
Bira Castro <bira.castro@gmail.com>
2020-08-05 13:38:38 -0300

    Ajuste do projeto Dominio - Pasta Correta

---
+++ Switch.Domain/Enums/SexoEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Enums
+{
+    public enum SexoEnum
+    {
+        Masculino = 1,
+        Feminino = 2
+
+
+    }
+}
