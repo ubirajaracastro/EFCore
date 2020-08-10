1be837d2ccd0f2e977ffc367e157d9f0d5d9dcae
Bira Castro <bira.castro@gmail.com>
2020-08-10 19:00:50 -0300

    Criação do Console de Teste do EF e Projeto de Loggin

---
+++ Switch.Console/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
+using Switch.Domain.Entities;
+using Switch.Infra.CrossCutting.Loggin;
+using Switch.Infra.Data.Context;
+using System;
+
+
+namespace Switch.APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usuario = new Usuario()
+            {
+                Nome="Mariana",
+                SobreNome="Souza",
+                Email="mariana.souza@gmail.com",
+                DataNascimento=DateTime.Now,
+                Sexo=Switch.Domain.Enums.SexoEnum.Masculino,
+                Senha ="vacaloca171"                            
+
+            };
+
+            var optionsBuilder = new DbContextOptionsBuilder<SwitchContext>();
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseMySql("Server=localhost;userid=root;password=vacaloca171;database=SwitchDB",
+                a => a.MigrationsAssembly("Switch.Infra.Data"));
+
+            try
+            {
+                var dbcontexto = new SwitchContext(optionsBuilder.Options);
+
+                //log
+                dbcontexto.GetService<ILoggerFactory>().AddProvider(new Logger());
+
+                dbcontexto.Add(usuario);
+                dbcontexto.SaveChanges();
+
+            }
+
+            catch(Exception ex)
+            {
+                string erro = ex.Message;
+
+            }
+
+            Console.ReadLine();
+
+
+            }
+    }
+}
--- Switch.Domain/Entities/Usuario.cs
+++ Switch.Domain/Entities/Usuario.cs
@@ -23,18 +23,18 @@ namespace Switch.Domain.Entities
         public string Senha { get; set; }
         public DateTime DataNascimento { get; set; }
         public SexoEnum Sexo { get; set; }
-        public string UrlFoto { get; set; }
-        public virtual ICollection<LocalTrabalho> LocaisTrabalho { get; set; }
-        public virtual ICollection<InstituicaoEnsino> InstituicoesEnsino { get; set; }
-
+        public string UrlFoto { get; set; }     
         public virtual StatusRelacionamento StatusRelacionamento { get; set; }
         public virtual ProcurandoPor ProcurandoPor { get; set; }
         public virtual Identificacao Identificacao { get; set; }
+
         public virtual ICollection<Postagem> Postagens { get; set; }
         public virtual ICollection<UsuarioGrupo> UsuarioGrupos { get; set; }
         public virtual ICollection<Amigo> Amigos { get; set; }
         public virtual ICollection<Comentario> Comentarios { get; set; }
-        
+        public virtual ICollection<LocalTrabalho> LocaisTrabalho { get; set; }
+        public virtual ICollection<InstituicaoEnsino> InstituicoesEnsino { get; set; }
+
 
 
     }
